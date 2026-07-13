4d5e97d4726843d81cc82502f612443c7bda5502
ChaunceyXv <ChaunceyXv@users.noreply.github.com>
2026-07-13 23:19:58 +0000

    style: v1.9.2 书签搜索框UI优化
    - 搜索框背景改为透明
    - 标题、搜索框、关闭按钮改为一行布局
    - 版本更新至 1.9.2

--- Custom-HomePage.js
+++ Custom-HomePage.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Custom HomePage
 // @namespace    https://github.com/user/Custom-HomePage
-// @version      1.9.1
+// @version      1.9.2
 // @description  自定义主页
 // @author       You
 // @match        *://*/*
